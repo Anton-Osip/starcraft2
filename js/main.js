fe87e3f6b3dc21450c54b8b11c22fb86f4571332
Anton-Osip <osipchika@gmail.com>
2023-03-15 11:22:13 +0300

    оптимизация

--- js/main.js
+++ js/main.js
@@ -188,23 +188,3 @@ let app = new Vue({
 		},
 	},
 })
-
-let audio = new Audio('audio/soundtrack.mp3')
-let audio_btn = document.querySelector('.btn-sound')
-let audio_icon = document.querySelector('.btn-sound i')
-
-audio.muted = true
-audio.autoplay = true
-audio.volume = 0.2
-
-audio_btn.addEventListener('click', function () {
-	if (audio.muted) {
-		audio.muted = false
-		audio_icon.classList.add('fa-volume-up')
-		audio_icon.classList.remove('fa-volume-off')
-	} else if (!audio.muted) {
-		audio.muted = true
-		audio_icon.classList.remove('fa-volume-up')
-		audio_icon.classList.add('fa-volume-off')
-	}
-})
